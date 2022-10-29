d58dd97347efa4800476b40adf3466db394ae4ff
Sarvar <arifdjanovsarvar@gmail.com>
2022-10-29 16:33:03 +0500

    Phase 2.1
    Setup a Recycle view for application.
    Got a list of labels for activities

---
+++ app/src/main/java/com/example/nerdlauncher_kotlin/NerdLauncherActivity.kt
@@ -0,0 +1,44 @@
+package com.example.nerdlauncher_kotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val  TAG = "NerdLauncherActivity"
+
+class NerdLauncherActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nerd_launcher)
+
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        setupAdapter()
+    }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+
+        val activities = packageManager.queryIntentActivities(startupIntent,0)
+
+
+        activities.sortWith(Comparator{a,b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        })
+
+        Log.i(TAG, "Found ${activities.size} activities")
+    }
+
+
+}
